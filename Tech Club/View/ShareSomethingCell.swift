23e4510f9587df2d88c421fdf394bae76684458a
Jessica Sendejo <jes.sendejo@gmail.com>
2021-05-10 23:50:33 -0700

    added firebase, changed view controllers to table view controllers

---
+++ Tech Club/View/ShareSomethingCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShareSomethingCell.swift
+//  Tech Club
+//
+//  Created by Jessica Sendejo on 5/10/21.
+//
+
+import UIKit
+
+class ShareSomethingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
